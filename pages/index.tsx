e6ef0bedd398dcf1a40ad937fd8cb4cb527fef91
Gillastre <jimmymention@gmail.com>
2022-04-27 06:43:23 +0200

    fixe build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect } from 'react'
-import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect } from 'react';
+import Layout from '../components/Layout';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 
