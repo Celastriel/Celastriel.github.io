4ef9545d35cae467fc8ef9105aaba6221ee8c25c
Gillastre <jimmymention@gmail.com>
2022-04-29 12:54:47 +0200

    update fixe nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,6 +4,7 @@ import { faHouse, faFolderTree, faIdCard } from '@fortawesome/free-solid-svg-ico
 import Head from 'next/head'
 import Link from 'next/link'
 import { useMediaQuery } from 'react-responsive'
+import Script from 'next/script'
 
 
 export default function NavBar(){
@@ -22,17 +23,10 @@ export default function NavBar(){
 
       return(<nav className="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-          <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+          <ul className="flex flex-row mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
-              <Link href={'/'}>
-                
-                <a className="block py-2 pl-3 pr-4 text-white border-b border-gray-100 hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0" aria-current="page">
+              <Link href={'/'}>              
+                <a className="block py-2 pl-3 pr-4 text-white hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0" aria-current="page">
                   <div className='flex flex-row items-center'>
                     <FontAwesomeIcon icon={faHouse} />
                     <span className='ml-5'>{tab.home}</span>
@@ -42,7 +36,7 @@ export default function NavBar(){
             </li>
             <li>
               <Link href={'/project'}>
-                <a className="block py-2 pl-3 pr-4 text-white border-b border-gray-100 hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0 ">
+                <a className="block py-2 pl-3 pr-4 text-white hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0 ">
                   <div className='flex flex-row items-center'>
                   <FontAwesomeIcon icon={faFolderTree} />
                     <span className='ml-5'>{tab.project}</span>
@@ -53,7 +47,7 @@ export default function NavBar(){
             <li>
               {isDesktopOrLaptop ?
               <Link href={'/resume'}>
-                <a className="block py-2 pl-3 pr-4 text-white border-b border-gray-100 hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0">
+                <a className="block py-2 pl-3 pr-4 text-white hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0">
                   <div className='flex flex-row items-center'>
                   <FontAwesomeIcon icon={faIdCard} />
                     <span className='ml-5'>{tab.resume}</span>
@@ -62,7 +56,7 @@ export default function NavBar(){
               </Link>
               :
               <Link href={'/files/CV.pdf'}>
-                <a className="block py-2 pl-3 pr-4 text-white border-b border-gray-100 hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0">
+                <a className="block py-2 pl-3 pr-4 text-white hover:bg-shade-light md:hover:bg-transparent md:border-0 md:hover:text-shade-light md:p-0">
                   <div className='flex flex-row items-center'>
                   <FontAwesomeIcon icon={faIdCard} />
                     <span className='ml-5'>{tab.resume}</span>
@@ -73,6 +67,5 @@ export default function NavBar(){
             </li>
           </ul>
         </div>
-      </div>
     </nav>)
 }
