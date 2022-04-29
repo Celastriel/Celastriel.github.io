bc210f1fcc31ee46b55a020aec397f5e5b43fc14
Gillastre <jimmymention@gmail.com>
2022-04-29 08:56:23 +0200

    NavBar Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,38 +7,15 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 
-  const balise : string = 'Présentation  </>';
-  const spitch : string = `Bonjour, je me présente.</br>
-J'ai commencé le développement en 2017 à la Haute école HELHa à Mons en Informatique de gestion.</br>
-Suite à ce Bachelier, j'ai également réaliser une formation Cefora en développement NodeJS.</br>
-Je suis activement à la recherche d'un emploi, </br>
-n'hésiter pas à consulter les projets que j'ai réaliser ainsi que mon CV
-`
-
   useEffect(()=>{
-    document.getElementsByClassName('spitch')[0].classList.remove('scale-0')
-    document.getElementsByClassName('spitch')[0].classList.add('scale-100')
-    document.getElementsByClassName('p-spitch')[0].innerHTML = spitch
-
-    document.getElementsByClassName('profil')[0].classList.remove('lg:-right-96')
-    document.getElementsByClassName('profil')[0].classList.add('lg:right-48')
 
   },[])
 
   return (
-    <Layout title="Accueil" currentPage="home">
-      <div className='flex lg:mt-12 lg:ml-24'>
-        <img src={process.env.PROFIL} width={288} height={64} className='absolute invisible transition-all duration-500 transform lg:rounded-full lg:ml-24 lg:visible lg:-right-96 -right-96 profil'/>
-        <div className='relative pl-10 my-auto italic text-white lg:mx-24 text-8xl'>
-            <h1>{process.env.NAME}</h1>
-            <p>{process.env.FUNCTION}</p>
-        </div>
-      </div>
-      <div className='p-6 text-xl transition-all duration-500 transform scale-0 bg-white border-8 border-dashed lg:pr-48 lg:mx-48 lg:mt-28 spitch'>
-        <h2 className='mb-4 text-4xl font-bold'>{balise}</h2>
-        <blockquote><p className='p-spitch'></p></blockquote>
-      </div>
-    </Layout> 
+    <Layout>
+
+      
+    </Layout>
   )
 }
 
